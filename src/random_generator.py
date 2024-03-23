a05bbb5151d07f5bc886ad87bef704bf8abbed1b
c0nD <colintiller21@gmail.com>
2024-03-23 16:20:21 -0400

    refactored

--- src/random_generator.py
+++ src/random_generator.py
@@ -13,3 +13,15 @@ def generate_random_color() -> tuple:
     b = random.random()
     a = 1.0
     return r, g, b, a
+
+
+def generate_random_star_color() -> tuple:
+    """
+    Generates a random color for a star.
+    returns a tuple representing the color -- specifically, a yellowish color.
+    """
+    r = random.random()
+    g = random.random() - 0.5
+    b = 0.0
+    a = 1.0
+    return r, g, b, a
